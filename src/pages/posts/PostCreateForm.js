4c684c7c8096f41f1224b8830683f82d283ebf8f
Niall Caplis <niallcaplis@gmail.com>
2024-11-10 15:37:17 +0000

    enable post creation form submission

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+import Asset from "../../components/Asset";
 import Upload from "../../assets/upload.png";
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import Asset from "../../components/Asset";
-import { Image } from "react-bootstrap";
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 
 function PostCreateForm() {
@@ -20,6 +23,10 @@ function PostCreateForm() {
         image: "",
     });
     const { title, post_text, image } = postData;
+
+    const imageInput = useRef(null);
+    const history = useHistory();
+
     const handleChange = (event) => {
         setPostData({
             ...postData,
@@ -35,6 +42,24 @@ function PostCreateForm() {
             });
         }
     };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append("title", title);
+        formData.append("post_text", post_text);
+        formData.append("image", imageInput.current.files[0]);
+        try {
+            const { data } = await axiosReq.post("/posts/", formData);
+            history.push(`/posts/${data.id}`);
+        } catch (err) {
+            console.log(err);
+            if (err.response?.status !== 401) {
+                setErrors(err.response?.data);
+            }
+        }
+    };
+
     const textFields = (
         <div className="text-center">
             <Form.Group>
@@ -46,6 +71,12 @@ function PostCreateForm() {
                     onChange={handleChange}
                 />
             </Form.Group>
+            {errors?.title?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
             <Form.Group>
                 <Form.Label>Text of Post</Form.Label>
                 <Form.Control
@@ -56,9 +87,15 @@ function PostCreateForm() {
                     onChange={handleChange}
                 />
             </Form.Group>
+            {errors?.post_text?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
             <Button
                 className={`${btnStyles.Button} ${btnStyles.Blue}`}
-                onClick={() => { }}
+                onClick={() => history.goBack()}
             >
                 cancel
             </Button>
@@ -67,8 +104,9 @@ function PostCreateForm() {
             </Button>
         </div>
     );
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Row>
                 <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
                     <Container
@@ -104,8 +142,15 @@ function PostCreateForm() {
                                 id="image-upload"
                                 accept="image/*"
                                 onChange={handleChangeImage}
+                                ref={imageInput}
                             />
                         </Form.Group>
+                        {errors?.image?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+
                         <div className="d-md-none">{textFields}</div>
                     </Container>
                 </Col>
