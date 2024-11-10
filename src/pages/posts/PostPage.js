7c9629bc0116d47cce960a52dd5b44efd2d8d0ab
Niall Caplis <niallcaplis@gmail.com>
2024-11-10 23:12:03 +0000

    add Post.js

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
 
 function PostPage() {
@@ -28,7 +29,7 @@ function PostPage() {
     return (
         <Row className="h-100">
             <Col className="mx-auto py-2 p-0 p-lg-2" lg={8}>
-                <p>Post component</p>
+                <Post {...post.results[0]} setPosts={setPost} postPage />
                 <Container className={appStyles.Content}>Comments</Container>
             </Col>
         </Row>
