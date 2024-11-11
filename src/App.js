ad9f978281f89e1c12cd98fb92157a1208c23eb3
Niall Caplis <niallcaplis@gmail.com>
2024-11-11 13:51:14 +0000

    add ProfileEditForm.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
 import ProfilePage from "./pages/profiles/ProfilePage";
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 
 
 function App() {
@@ -79,6 +82,21 @@ function App() {
                         exact
                         path="/profiles/:id"
                         render={() => <ProfilePage />}
+                    />
+                    <Route
+                        exact
+                        path="/profiles/:id/edit/username"
+                        render={() => <UsernameForm />}
+                    />
+                    <Route
+                        exact
+                        path="/profiles/:id/edit/password"
+                        render={() => <UserPasswordForm />}
+                    />
+                    <Route
+                        exact
+                        path="/profiles/:id/edit"
+                        render={() => <ProfileEditForm />}
                     />
                     <Route
                         render={() => <p>Sorry, that page doesn't exist.</p>}
