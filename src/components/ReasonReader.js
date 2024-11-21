d3cc181adb8e1f7637ddbc79cbddbb8c421d414b
Niall Caplis <niallcaplis@gmail.com>
2024-11-21 11:25:35 +0000

    fix ReasonReader component

--- src/components/ReasonReader.js
+++ src/components/ReasonReader.js
@@ -1,26 +1,37 @@
-const ReasonReader = (reasonNum) => {
+function ReasonReader ({reasonNum}) {
 
     let reasonText;
 
-    switch(reasonNum) {
+    switch(parseInt(reasonNum)) {
         case 1:
             reasonText = "Graphic violence";
+            break;
         case 2:
             reasonText = "Explicit sexual content";
+            break;
         case 3:
             reasonText = "Sexualization of minors";
+            break;
         case 4:
             reasonText = "Inciting hatred";
+            break;
         case 5:
             reasonText = "Encouraging suicide or self-harm";
+            break;
         case 6:
             reasonText = "Attempting to defraud";
+            break;
         case 7:
             reasonText = "Advertising illegal products";
+            break;
         case 8:
             reasonText = "Blatant copyright infringement";
+            break;
         case 9:
             reasonText = "Other serious reason";
+            break;
+        default:
+            reasonText = "Reason integer could not be interpreted";
     }
 
     return reasonText;
