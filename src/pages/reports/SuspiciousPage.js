3af65b7da61006e94a91e8875cf65eadb987a243
Niall Caplis <niallcaplis@gmail.com>
2024-11-21 11:52:20 +0000

    add Card elements to structure SuspiciousPage

--- src/pages/reports/SuspiciousPage.js
+++ src/pages/reports/SuspiciousPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Card from "react-bootstrap/Card";
 
 import { axiosReq } from "../../api/axiosDefaults";
 import logo from "../../assets/logo.png";
@@ -11,6 +12,9 @@ function SuspiciousPage() {
     const [reports, setReports] = useState({ results: [] });
 
     useEffect(() => {
+        /**
+         * Fetches Report instances
+        */
         const fetchReports = async () => {
             try {
                 const { data } = await axiosReq.get("/suspicious");
@@ -27,41 +31,45 @@ function SuspiciousPage() {
         <>
             {reports.results.length ? (
 
-                <>
-                    <table>
-                        <tr>
-                            <th>Reason</th>
-                            <td>
-                                <ReasonReader
-                                    reasonNum={reports.results[0].reason}
+                <Card>
+                    <Card className={styles.Card}>
+                        <table>
+                            <tr>
+                                <th>Reason</th>
+                                <td>
+                                    <ReasonReader
+                                        reasonNum={reports.results[0].reason}
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Explanation</th>
+                                <td>{reports.results[0].explanation}</td>
+                            </tr>
+                        </table>
+                    </Card>
+                    <Card className={styles.Card}>
+                        <section id="reported-post">
+                            <figure>
+                                <img
+                                    src={reports.results[0].post_image}
+                                    alt="from reported post"
+                                    id="r-post-image"
                                 />
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>Explanation</th>
-                            <td>{reports.results[0].explanation}</td>
-                        </tr>
-                    </table>
+                            </figure>
 
-                    <section id="reported-post">
-                        <figure>
-                            <img
-                                src={reports.results[0].post_image}
-                                alt="image from reported post"
-                                id="r-post-image"
-                            />
-                        </figure>
-
-                        <article id="r-post-text-fields">
-                            <h5>{reports.results[0].post_title}</h5>
-                            <p>{reports.results[0].post_text}</p>
-                        </article>
-                    </section>
-
-                    <p id="sus-count">
-                        <b>{reports.results.length}</b> suspicious posts to review
-                    </p>
-                </>
+                            <article id="r-post-text-fields">
+                                <h5>{reports.results[0].post_title}</h5>
+                                <p>{reports.results[0].post_text}</p>
+                            </article>
+                        </section>
+                    </Card>
+                    <Card className={`${styles.Card} ${styles.SusCount}`}>
+                        <p>
+                            <b>{reports.results.length}</b> suspicious posts to review
+                        </p>
+                    </Card>
+                </Card>
 
             ) : (
 
